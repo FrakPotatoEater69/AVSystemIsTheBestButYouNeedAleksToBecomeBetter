6bf2ae97290dbc7edcc6a9e84fc5d43235564252
Aliaksey Shakhner <shahner.lesha@gmail.com>
2024-05-25 20:16:59 +0200

    Add ElevatorStatus class

---
+++ src/main/java/com/shakhner/model/ElevatorStatus.java
@@ -0,0 +1,25 @@
+package com.shakhner.model;
+
+public class ElevatorStatus {
+    private Integer elevatorId;
+    private Integer currentFloor;
+    private Integer targetFloor;
+
+    public ElevatorStatus(Integer elevatorId, Integer currentFloor, Integer targetFloor) {
+        this.elevatorId = elevatorId;
+        this.currentFloor = currentFloor;
+        this.targetFloor = targetFloor;
+    }
+
+    public Integer getCurrentFloor() {
+        return currentFloor;
+    }
+
+    public Integer getTargetFloor() {
+        return targetFloor;
+    }
+
+    public String toString() {
+        return "[" + elevatorId + "]  | " + currentFloor + " | " + "  | " + targetFloor + " |";
+    }
+}
