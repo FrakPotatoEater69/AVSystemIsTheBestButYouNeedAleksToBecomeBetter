39e8abfc9d607b6384f0cd499b513564727313fb
Aliaksey Shakhner <shahner.lesha@gmail.com>
2024-05-25 20:02:40 +0200

    Initialize Elevator class

--- src/main/java/com/shakhner/model/Elevator.java
+++ src/main/java/com/shakhner/model/Elevator.java
@@ -1,4 +1,22 @@
 package com.shakhner.model;
 
+import com.shakhner.commands.ElevatorCommand;
+import com.shakhner.states.ElevatorState;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
 public class Elevator {
+    private Integer id;
+    private ElevatorState elevatorState;
+    private Integer currentFloor;
+    private Integer nextDirection;
+    private Deque<ElevatorCommand> commands = new LinkedList<>();
+
+    public Elevator(int id, int initialFloor) {
+        this.id = id;
+        this.currentFloor = initialFloor;
+        //Initialize state!
+        //Add Status!
+    }
 }
