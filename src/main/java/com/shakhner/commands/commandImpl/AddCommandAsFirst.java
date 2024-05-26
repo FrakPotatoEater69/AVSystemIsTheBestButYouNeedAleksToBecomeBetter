3282e5160d73a6c3623b40e4bf9036dd6d5f6be4
Aliaksey Shakhner <shahner.lesha@gmail.com>
2024-05-26 16:46:07 +0200

    Add new ElevatorCommand AddCommantAsFirst

---
+++ src/main/java/com/shakhner/commands/commandImpl/AddCommandAsFirst.java
@@ -0,0 +1,32 @@
+package com.shakhner.commands.commandImpl;
+
+import com.shakhner.commands.ElevatorCommand;
+import com.shakhner.models.Elevator;
+import com.shakhner.states.ElevatorState;
+
+public class AddCommandAsFirst implements ElevatorCommand {
+    ElevatorCommand command;
+
+    public AddCommandAsFirst(ElevatorCommand command) {
+        this.command = command;
+    }
+    @Override
+    public void execute(Elevator elevator) {
+        elevator.addCommandAsFirst(command);
+    }
+
+    @Override
+    public void setElevatorState(Elevator elevator) {
+
+    }
+
+    @Override
+    public Integer getFloorNumber() {
+        return null;
+    }
+
+    @Override
+    public void setFloorNumber(Integer floorNumber) {
+
+    }
+}
